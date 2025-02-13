c9c16803d80939b04d6df35c4ff207108d961ddf
Ivan Arefiev <53241503+Jet1902@users.noreply.github.com>
2025-02-13 22:03:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <QDebug>
 #include <QRandomGenerator>
 #include <QTime>
+//#include <QThread>
+#include <thread>
+#include <mutex>
+#include <iostream>
 
 #include "arraysort.h"
 
@@ -12,11 +16,16 @@ void printArrayCheck(bool isSorted);
 
 typedef void (*sortfunc)(int *pArray, unsigned int size);
 
-void testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func);
+int testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func);
+
+static std::mutex mtx;                  // Объявление мьютекса для защиты разделяемых данных
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    std::vector<std::thread> vTh;       // Вектор потоков
+    int execTime = 0;                   // Переменная для хранения суммы времени выполнения потоков
 
     typedef QMap<QString, sortfunc> vFunc;
     vFunc vFunctions;
@@ -39,16 +48,49 @@ int main(int argc, char *argv[])
         printArray(pOriginal, iSize);
     }
 
+    int execStart = QTime::currentTime().msecsSinceStartOfDay();
+
     for(vFunc::iterator it = vFunctions.begin(); it != vFunctions.end(); ++it)
-        testSortFunc(pOriginal,iSize, it.key(), it.value());
+    {
+        auto f = [&execTime](int * pOriginal, unsigned int iSize, QString name, sortfunc func)      // Лямбда-функция, добавляющая время выполнения потока в execTime
+        {
+            int time = testSortFunc(pOriginal, iSize, name, func);                                  // и вызывающая функцию сортировки
+            mtx.lock();                                                                             // Лочим использование ресурса execTime
+            execTime += time;
+            mtx.unlock();                                                                           // Ресурс разблокирован
+
+//            {
+//                std::lock_guard<std::mutex> locker(mtx);                                          // Реализация того же самого с помощью lock_guard
+//                execTime += time;                                                                 // Ограничение области видимости диктует область работы lock_guard
+//            }
+
+//            std::unique_lock<std::mutex> ul(mtx);                                                 // Опять то же самое
+//            execTime += time;
+//            ul.unlock();                                                                          // Если бы не освободили,
+                                                                                                    // он бы осводил сам при выходе из области видимости
+        };
+        vTh.push_back(std::thread(f, pOriginal, iSize, it.key(), it.value()));                      // Создание потока, запихивание туда лямбда-функции
+                                                                                                    // и запихивание самого потока в вектор потоков
+                                                                                                    // Лямбда-функция: [передача ресурсов] (параметры){тело}
+    }
+
 
     delete [] pOriginal;
 
+    for(auto &th : vTh)                                                                             // Ждем каждый поток в векторе
+        th.join();
+
+    int execFinish = QTime::currentTime().msecsSinceStartOfDay();
+
+    std::cout << "Execution Time: " << execFinish - execStart << " ms\n";
+    std::cout << "Summ of threads time: " << execTime << " ms\n";
+    std::cout << "Time saved: " << execTime - (execFinish - execStart) << " ms\n";
     return a.exec();
 }
 
-void testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func)
+int testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func)
 {
+
     int *pTest = new int[size];
     memcpy(pTest, pArray, sizeof(int) * size);
 
@@ -56,6 +98,7 @@ void testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func)
     (*func)(pTest, size);
     int finish = QTime::currentTime().msecsSinceStartOfDay();
 
+    std::cout << "Thread number: " << std::this_thread::get_id() << "\n";           // Вывод номера потока
     qDebug() << name << (isSortedArray(pTest, size) ? " success" : "failure") << ", took" << finish - start << "ms";
 
     if(size <= 10)
@@ -65,6 +108,8 @@ void testSortFunc(int *pArray, unsigned int size, QString name, sortfunc func)
     }
 
     delete [] pTest;
+
+    return finish - start;
 }
 
 // Заполнение массива  случайными величинами
